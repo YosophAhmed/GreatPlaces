b4d891fa3e45927bcd9078d06cf0f326452c7d3b
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-26 08:03:09 +0200

    Creating Project

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Great Places',
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(
+              Icons.add,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
