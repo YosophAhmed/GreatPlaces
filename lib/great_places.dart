f65d9d47a2cf353064f2909ead1f9c687b213775
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-26 09:04:17 +0200

    Adding Screens Routes

--- lib/great_places.dart
+++ lib/great_places.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/places.dart';
+import 'package:great_places/screens/add_place_screen.dart';
 import 'package:great_places/screens/places_list_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
@@ -14,10 +15,15 @@ class GreatPlaces extends StatelessWidget {
       child: Sizer(
         builder: (context, orientation, deviceType) {
           return MaterialApp(
+            debugShowCheckedModeBanner: false,
             theme: ThemeData(
               primarySwatch: Colors.indigo,
             ),
-            home: const PlacesListScreen(),
+            routes: {
+              PlacesListScreen.routeName : (context) => const PlacesListScreen(),
+              AddPlaceScreen.routeName : (context) => const AddPlaceScreen(),
+            },
+            initialRoute: PlacesListScreen.routeName,
           );
         },
       ),
