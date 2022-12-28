ca8be7c527ad8e0e4ab6563fb980aae51cf965cf
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-28 10:35:57 +0200

    Display Saved Places

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:great_places/screens/add_place_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/places.dart';
 
 class PlacesListScreen extends StatelessWidget {
   static const String routeName = 'PlacesListScreen';
@@ -28,8 +31,32 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<Places>(
+        builder: (context, provider, child) {
+          if (provider.items.isEmpty) {
+            return child!;
+          } else {
+            return ListView.builder(
+              itemBuilder: (context, index) => ListTile(
+                title: Text(
+                  provider.items[index].title,
+                ),
+                leading: CircleAvatar(
+                  backgroundImage: FileImage(
+                    provider.items[index].image,
+                  ),
+                ),
+                onTap: (){},
+              ),
+              itemCount: provider.items.length,
+            );
+          }
+        },
+        child: const Center(
+          child: Text(
+            'No Places yet, Start adding some places!',
+          ),
+        ),
       ),
     );
   }
