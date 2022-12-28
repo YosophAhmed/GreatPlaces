2e05e7abc730b87cdad474ff2aa46a2db50601b7
Yosoph Ahmed <devyosoph@gmail.com>
2022-12-28 10:36:16 +0200

    Adding addPlace method

--- lib/providers/places.dart
+++ lib/providers/places.dart
@@ -1,11 +1,26 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 import '../models/place_model.dart';
 
 class Places with ChangeNotifier {
-  List<Place> _items = [];
+  List<Place> items = [];
 
-  List<Place> get items {
+  List<Place> get getItems {
     return [...items];
+  }
+
+  void addPlace({
+    required String title,
+    required File image,
+  }) {
+    final newPlace = Place(
+      id: DateTime.now().toString().substring(0, 11),
+      title: title,
+      image: image,
+    );
+    items.add(newPlace);
+    notifyListeners();
   }
 }
